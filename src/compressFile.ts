f5e769b3392426d1eeb971a9fb5ba7b41cbe30e7
sharonYoobic <shophy@yoobic.com>
2022-05-30 10:10:15 +0300

    fix(app): Compress not awaiting write finish

--- src/compressFile.ts
+++ src/compressFile.ts
@@ -1,7 +1,7 @@
 import * as fs from 'fs';
 import * as zlib from 'zlib';
 
-function compressFile(filename: string): Promise<void> {
+async function compressFile(filename: string): Promise<void> {
     const tempFilename = `${filename}.temp`;
 
     fs.renameSync(filename, tempFilename);
@@ -20,7 +20,7 @@ function compressFile(filename: string): Promise<void> {
         const write = fs.createWriteStream(filename);
         read.pipe(zip).pipe(write);
 
-        return new Promise((resolve, reject) => {
+        return await new Promise((resolve, reject) => {
             write.on(
                 'error',
                 /* istanbul ignore next */ err => {
